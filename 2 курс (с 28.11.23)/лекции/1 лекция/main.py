19a61f18ddf676e26db2c4f972539f00f996806d
Ksenia <418875603@mail.ru>
2023-11-28 23:58:59 +0300

    meeting with python

---
+++ 2 курс (с 28.11.23)/лекции/1 лекция/main.py
@@ -0,0 +1,44 @@
+# c = 2
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# print('enter first number:')
+# a = int(input())
+# b = int(input('enter second number: '))
+# print (a, '+', b, '=', a+b)
+
+# n = 257
+# summa = 0
+# while n>0:
+#     x = n%10
+#     summa += x
+#     n = n//10
+# print(summa)
+
+# n = int (input())
+# flag = True
+# i = 2
+# while flag:
+#     if n%i == 0:
+#         flag = False
+#         print(i)
+#     elif i>n//2:
+#         print(n)
+#         flag = False
+#     i += 1
+
+# r = range(0, -5, -1)
+# for i in r:
+#     print(i)
+# for i in '[f.irb':
+#     print(i)
+
+line = ""
+for i in range(5):
+    line = ""
+    for j in range(12):
+        line += "*"
+    print(line)
