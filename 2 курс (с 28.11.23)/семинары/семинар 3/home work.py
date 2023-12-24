4587bcbb6cde84f7ee4bfd697a201ad295e38fbd
Ksenia <418875603@mail.ru>
2023-12-24 23:48:15 +0300

    hw 3

--- 2 курс (с 28.11.23)/семинары/семинар 3/home work.py
+++ 2 курс (с 28.11.23)/семинары/семинар 3/home work.py
@@ -46,14 +46,65 @@
 # слова k и выводит его. Будем считать, что на вход подается только одно слово, 
 # которое содержит либо только английские, либо только русские буквы.
 
-new_list = str(input('enter word in Rus or Eng'))
+# new_list = str(input('enter word in Rus or Eng'))
 
-new_dicts = {'1': 'A, E, I, O, U, L, N, S, T, R, А, В, Е, И, Н, О, Р, С, Т'}, {'2' : 'D, G, Д, К, Л, М, П, У'},
-{'3':'B, C, M, P, Б, Г, Ё, Ь, Я'}, {'4': 'F, H, V, W, Y, Й, Ы'}, {'5': 'K, Ж, З, Х, Ц, Ч'}, 
-{'8': 'J, X, Ш, Э, Ю'}, {'10': 'Q, Z, Ф, Щ, Ъ'}
+# text = 'ноутбук'
+
+# new_dicts = {1: 'AEIOULNSTRАВЕИНОРСТ', 2: 'DGДКЛМПУ',
+# 3:'BCMPБГЁЬЯ', 4: 'FHVWYЙЫ', 5: 'KЖЗХЦЧ', 
+# 8: 'JXШЭЮ', 10: 'QZФЩЪ'}
     
-count = 0
+# count = 0
 
-for dict in new_dicts:
-    for element in new_list:
+# if text[0].upper() in new_dicts:
+#     for key, value in new_dicts.items():
+#         count+= key
+    
+
+
+
+# # if word[0].lower() in eng:
+# #     summa = 0
+# #     for letter in word:
+# #         for key, value in list_English.items():
+# #             if letter.upper() in value:
+# #                 summa += key
+#     print(f'стоимость слова = {count}')
+
+# # Введите ваше решение ниже
+
+# # dict = {"AEIOULNSTRАВЕИНОРСТ":1, "DGДКЛМПУ":2,"BCMPБГЁЬЯ":3,"FHVWYЙЫ":4,"KЖЗХЦЧ":5,"JXШЭЮ":8,"QZФЩЪ":10}
+# # print(sum([k for i in k]))
         
+# # import re
+# # def isCyrillic(text):
+# # 	return bool(re.search('[а-яА-Я]', text))
+# # points_en = {1:'AEIOULNSTR',
+# #       	2:'DG',
+# #       	3:'BCMP',
+# #       	4:'FHVWY',
+# #       	5:'K',
+# #       	8:'JZ',
+# #       	10:'QZ'}
+# # points_ru = {1:'АВЕИНОРСТ',
+# #       	2:'ДКЛМПУ',
+# #       	3:'БГЁЬЯ',
+# #       	4:'ЙЫ',
+# #       	5:'ЖЗХЦЧ',
+# #       	8:'ШЭЮ',
+# #       	10:'ФЩЪ'}
+# # text = 'ноутбук'
+# # if isCyrillic(text):
+# # 	print(sum([k for i in text for k, v in points_ru.items() if i in v]))
+# # else:
+# # 	print(sum([k for i in text for k, v in points_en.items() if i in v]))
+
+# count = 0
+# new_dicts = {1: 'AEIOULNSTRАВЕИНОРСТ', 2: 'DGДКЛМПУ',
+# 3:'BCMPБГЁЬЯ', 4: 'FHVWYЙЫ', 5: 'KЖЗХЦЧ', 
+# 8: 'JXШЭЮ', 10: 'QZФЩЪ'}
+
+# for key in new_dicts:
+#     for text[0] in key[value]:
+#         count += key
+# print(count)
